ad62aab622a2fdaab6261842c750dc24ee6118d8
liviaab <livia.almeida@appprova.com.br>
2019-10-19 13:20:48 -0300

    Add base_url constant

--- module/main/crawler.py
+++ module/main/crawler.py
@@ -4,6 +4,8 @@ PROCESS_NUMBER_LENGTH = 25
 UNIFIED_DIGIT_AND_YEAR_INDEX = 15
 UNIFIED_FORUM_INDEX = 21
 
+BASE_URL = 'https://www2.tjal.jus.br/cpopg/search.do'
+
 default_payload = {
     'conversationId': '',
     'dadosConsulta.localPesquisa.cdLocal': '-1',
@@ -15,8 +17,7 @@ default_payload = {
 
 
 class CourtCrawler:
-    def __init__(self, base_url='https://www2.tjal.jus.br/cpopg/search.do',
-                payload=default_payload):
+    def __init__(self, base_url=BASE_URL, payload=default_payload):
         self.base_url = base_url
         self.payload = payload
 
