f00cf9e5d9b9e4e84bc42944e98113ece4ae437e
liviaab <livia.almeida@appprova.com.br>
2019-10-20 00:21:37 -0300

    Add setup.py to import modules from outer directories

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Court Crawler",
+    version="0.1",
+    packages=find_packages(),
+    author="Lívia Almeida Barbosa",
+    description="This project is the backend of an application used to retrieve information of specifics lawsuits from a brazilian court of justice, TJAL.",
+    url="https://github.com/liviaab/backend-crawler",
+)
